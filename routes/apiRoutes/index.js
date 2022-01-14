f32576e7095fa582d5af634f1c77ed005e697e58
tkcannon <taylorkcannon@gmail.com>
2022-01-14 11:39:52 -0700

    added votes route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,4 +4,5 @@ const router = express.Router();
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 module.exports = router;
